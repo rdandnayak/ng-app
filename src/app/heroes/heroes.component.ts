722317d9e400ff20f8c91ec92f254b94004c146b
Rohit Dandnayak <rohit.dandnayak@soroco.com>
2019-04-25 11:20:15 +0530

    master/detail component done

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+  constructor() {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  ngOnInit() {}
+}
